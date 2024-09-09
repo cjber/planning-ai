eacb15507b323b727af3db445d5b6ec5662ac93a
cjber <cjberragan@gmail.com>
2024-09-09 12:43:46 +0100

    allow for structured outputs

--- planning_ai/chains/map_chain.py
+++ planning_ai/chains/map_chain.py
@@ -1,13 +1,41 @@
-from langchain_core.output_parsers import StrOutputParser
+from typing import Literal, Optional
+
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field, validator
 
 from planning_ai.llms.llm import LLM
 
 with open("./planning_ai/chains/prompts/map.txt", "r") as f:
     map_template = f.read()
 
+
+class BriefSummary(BaseModel):
+    """A summary of the response with generated metadata"""
+
+    summary: str = Field(..., description="Summary of the response.")
+    stance: Literal["SUPPORT", "OPPOSE", "NEUTRAL"] = Field(
+        ..., description="Overall stance of the response."
+    )
+    themes: list[str] = Field(
+        ..., description="A list of themes associated with the response."
+    )
+    rating: int = Field(
+        ...,
+        description="How constructive the response is, from a rating of 1 to 10.",
+    )
+
+    @validator("summary")
+    def summary_must_not_be_empty(cls, v):
+        if not v.strip():
+            raise ValueError("Summary cannot be empty.")
+        return v
+
+
+SLLM = LLM.with_structured_output(BriefSummary)
+
+
 map_prompt = ChatPromptTemplate.from_messages([("system", map_template)])
-map_chain = map_prompt | LLM | StrOutputParser()
+map_chain = map_prompt | SLLM
 
 if __name__ == "__main__":
     test_document = """
@@ -21,3 +49,4 @@ if __name__ == "__main__":
 
     print("Generated Summary:")
     print(result)
+    result
