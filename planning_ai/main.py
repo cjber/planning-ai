f3f77cd3473310f25216ef57b0ab6a17d0c9546e
cjber <cjberragan@gmail.com>
2024-09-09 11:51:02 +0100

    refactor main to function

--- planning_ai/main.py
+++ planning_ai/main.py
@@ -4,25 +4,35 @@ from langchain_text_splitters import CharacterTextSplitter
 from planning_ai.common.utils import Paths
 from planning_ai.graph import create_graph
 
-loader = DirectoryLoader(
-    path=str(Paths.STAGING),
-    show_progress=True,
-    use_multithreading=True,
-    loader_cls=TextLoader,
-    recursive=True,
-)
-docs = [doc for doc in loader.load() if doc.page_content]
-text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=1000, chunk_overlap=0
-)
-split_docs = text_splitter.split_documents(docs)
 
-app = create_graph()
+def main():
+    loader = DirectoryLoader(
+        path=str(Paths.STAGING),
+        show_progress=True,
+        use_multithreading=True,
+        loader_cls=TextLoader,
+        recursive=True,
+    )
+    docs = [doc for doc in loader.load() if doc.page_content]
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    split_docs = text_splitter.split_documents(docs)
 
-for step in app.stream(
-    {"contents": [doc.page_content for doc in split_docs]},
-    {"recursion_limit": 10},
-):
-    print(list(step.keys()))
+    app = create_graph()
 
-print(step["generate_final_summary"]["final_summary"])
+    step = None
+    for step in app.stream(
+        {"contents": [doc.page_content for doc in split_docs]},
+        {"recursion_limit": 10},
+    ):
+        print(list(step.keys()))
+
+    if step is None:
+        raise ValueError("No steps were processed!")
+
+    return step
+
+
+if __name__ == "__main__":
+    out = main()
