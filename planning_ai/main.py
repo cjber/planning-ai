ce00ddf15dad20ecc52855ec9e9eefe2f31b0e33
cjber <cjberragan@gmail.com>
2024-09-10 12:59:28 +0100

    build document using quarto

--- planning_ai/main.py
+++ planning_ai/main.py
@@ -1,8 +1,115 @@
+from collections import Counter
+from pathlib import Path
+
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 
 from planning_ai.common.utils import Paths
 from planning_ai.graph import create_graph
+
+doc_title = "Cambridge Response Summary"
+
+
+def build_quarto_doc(doc_title, out):
+    final = out["generate_final_summary"]
+    executive_summary = (
+        final["final_summary"].split("## Key points raised in support")[0].strip()
+    )
+    key_points = final["final_summary"].split("## Key points raised in support")[1]
+
+    themes = [
+        theme
+        for summary in final["collapsed_summaries"]
+        for theme in summary.metadata["themes"]
+    ]
+    value_counts = Counter(themes)
+    total_values = sum(value_counts.values())
+    percentages = {
+        key: {"count": count, "percentage": (count / total_values)}
+        for key, count in value_counts.items()
+    }
+    top_5 = sorted(percentages.items(), key=lambda x: x[1]["percentage"], reverse=True)[
+        :5
+    ]
+    thematic_breakdown = "| **Theme** | **Percentage** | **Count** |\n|---|---|---|\n"
+    thematic_breakdown += "\n".join(
+        [f"| {item} | {d['percentage']:.2%} | {d['count']} |" for item, d in top_5]
+    )
+    places = [
+        place
+        for summary in final["collapsed_summaries"]
+        for place in summary.metadata["places"]
+    ]
+    value_counts = Counter(places)
+    total_values = sum(value_counts.values())
+    percentages = {
+        key: {"count": count, "percentage": (count / total_values)}
+        for key, count in value_counts.items()
+    }
+    top_5 = sorted(percentages.items(), key=lambda x: x[1]["percentage"], reverse=True)[
+        :5
+    ]
+    places_breakdown = "| **Place** | **Percentage** | **Count** |\n|---|---|---|\n"
+    places_breakdown += "\n".join(
+        [f"| {item} | {d['percentage']:.2%} | {d['count']} |" for item, d in top_5]
+    )
+
+    stances = [summary.metadata["stance"] for summary in final["collapsed_summaries"]]
+    value_counts = Counter(stances)
+    total_values = sum(value_counts.values())
+    percentages = {
+        key: {"count": count, "percentage": (count / total_values)}
+        for key, count in value_counts.items()
+    }
+    stances_top = sorted(
+        percentages.items(), key=lambda x: x[1]["percentage"], reverse=True
+    )
+    stances_breakdown = " | ".join(
+        [
+            f"**{item}**: {stance['percentage']:.2%} _({stance['count']})_"
+            for item, stance in stances_top
+        ]
+    )
+
+    short_summaries = "\n\n".join(
+        [
+            f"#### {summary.metadata['filename']}\n"
+            f"{summary.page_content}\n\n"
+            f"**Stance**: {summary.metadata['stance']}\n\n"
+            f"**Constructiveness**: {summary.metadata['rating']}\n\n"
+            for summary in final["collapsed_summaries"]
+        ]
+    )
+
+    quarto_doc = (
+        "---\n"
+        f"title: '{doc_title}'\n"
+        "format:\n"
+        "  PrettyPDF-pdf:\n"
+        "    papersize: A4\n"
+        "execute:\n"
+        "  freeze: auto\n"
+        "  echo: false\n"
+        "monofont: 'JetBrains Mono'\n"
+        "monofontoptions:\n"
+        "  - Scale=0.55\n"
+        "---\n\n"
+        f"{executive_summary}\n\n"
+        f"{stances_breakdown}\n\n"
+        "## Thematic Breakdown\n\n"
+        "The thematic breakdown identifies key topics that are mentioned \n"
+        "within each response. These topics may be discussed either in support or in \n"
+        "opposition of the plan. A single response may discuss multiple topics.\n"
+        f"\n\n{thematic_breakdown}\n\n"
+        f"\n\n{places_breakdown}\n\n"
+        "## Key points raised in support\n\n"
+        f"{key_points}\n\n"
+        "## Summaries\n"
+        f"{short_summaries}"
+    )
+
+    with open(f"./reports/{doc_title.replace(' ', '_')}.qmd", "w") as f:
+        f.write(quarto_doc)
 
 
 def main():
@@ -13,8 +120,7 @@ def main():
         loader_cls=TextLoader,
         recursive=True,
     )
-    docs = [doc for doc in loader.load()[10:30] if doc.page_content]
-    # TEMP: limit docs
+    docs = [doc for doc in loader.load()[:200] if doc.page_content]
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000, chunk_overlap=0
     )
@@ -24,7 +130,10 @@ def main():
 
     step = None
     for step in app.stream(
-        {"contents": [doc.page_content for doc in split_docs]},
+        {
+            "contents": [doc.page_content for doc in split_docs],
+            "filenames": [Path(doc.metadata["source"]).stem for doc in split_docs],
+        },
         {"recursion_limit": 10},
     ):
         print(list(step.keys()))
@@ -37,21 +146,4 @@ def main():
 
 if __name__ == "__main__":
     out = main()
-    quarto_doc = (
-        """---
-title: "Semantic Data Catalogue"
-format: 
-  PrettyPDF-pdf:
-        papersize: A4
-execute:
-  freeze: auto
-  echo: false
-monofont: 'JetBrains Mono'
-monofontoptions: 
-  - Scale=0.55
----\n\n"""
-        + out["generate_final_summary"]["final_summary"]
-    )
-
-    with open("./reports/DEMO_REPORT.qmd", "w") as f:
-        f.write(quarto_doc)
+    build_quarto_doc(doc_title, out)
