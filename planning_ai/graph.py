8f956e955f2c3c2a35c511733e5c5aaf0e6b4b69
cjber <cjberragan@gmail.com>
2024-09-13 12:04:11 +0100

    feat: add hallucination handling nodes and update graph structure

--- planning_ai/graph.py
+++ planning_ai/graph.py
@@ -1,5 +1,11 @@
-from langgraph.graph import END, START, StateGraph
+from langgraph.graph import END, StateGraph
 
+from planning_ai.nodes.hallucination_node import (
+    check_hallucination,
+    fix_hallucination,
+    map_fix_hallucinations,
+    map_hallucinations,
+)
 from planning_ai.nodes.map_node import (
     collect_summaries,
     generate_summary,
@@ -12,12 +18,24 @@ from planning_ai.states import OverallState
 def create_graph():
     graph = StateGraph(OverallState)
     graph.add_node("generate_summary", generate_summary)
+    graph.add_node("check_hallucination", check_hallucination)
+    graph.add_node("fix_hallucination", fix_hallucination)
     graph.add_node("collect_summaries", collect_summaries)
     graph.add_node("generate_final_summary", generate_final_summary)
 
-    graph.add_conditional_edges(START, map_summaries, ["generate_summary"])
-    graph.add_edge("generate_summary", "collect_summaries")
+    graph.set_conditional_entry_point(map_summaries, ["generate_summary"])
+    graph.add_conditional_edges(
+        "generate_summary", map_hallucinations, ["check_hallucination"]
+    )
+    graph.add_conditional_edges(
+        "check_hallucination", map_fix_hallucinations, ["fix_hallucination"]
+    )
+    graph.add_edge("fix_hallucination", "check_hallucination")
+    graph.add_edge("fix_hallucination", "collect_summaries")
     graph.add_edge("collect_summaries", "generate_final_summary")
-    graph.add_edge("generate_final_summary", END)
+    # graph.add_edge("generate_final_summary", END)
 
     return graph.compile()
+
+
+print(create_graph().get_graph().draw_ascii())
