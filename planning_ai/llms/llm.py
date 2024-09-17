715c4efd0aa66423bbd14c00675fa9422bd6f97a
cjber <cjberragan@gmail.com>
2024-09-17 14:50:42 +0100

    increase rate of openai llm

--- planning_ai/llms/llm.py
+++ planning_ai/llms/llm.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 rate_limiter = InMemoryRateLimiter(
-    requests_per_second=5,
+    requests_per_second=50,
     check_every_n_seconds=0.1,
 )
 LLM = ChatOpenAI(temperature=0, model="gpt-4o-mini", rate_limiter=rate_limiter)
