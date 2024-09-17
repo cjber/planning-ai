faae69b430e0c305582e845396e7f677efecbd1a
cjber <cjberragan@gmail.com>
2024-09-17 16:01:28 +0100

    keep aims in summaries

--- planning_ai/nodes/reduce_node.py
+++ planning_ai/nodes/reduce_node.py
@@ -5,7 +5,7 @@ from planning_ai.states import OverallState
 def generate_final_summary(state: OverallState):
     if len(state["documents"]) == len(state["summaries_fixed"]):
         summaries = [
-            summary["summary"].summary
+            str(summary["summary"])
             for summary in state["summaries_fixed"]
             if summary["summary"].stance != "NEUTRAL" and summary["summary"].rating >= 5
         ]
@@ -17,12 +17,3 @@ def generate_final_summary(state: OverallState):
             "hallucinations": state["hallucinations"],
             "documents": state["documents"],
         }
-
-
-def add_snippets(state: OverallState):
-    final_summary = state["final_summary"]
-    summaries = state["summaries_fixed"]
-
-    response = snippet_chain.invoke(
-        {"final_summary": final_summary, "summaries": summaries}
-    )
