197ccec9c8615aa563481b5d99b644292cfcb560
cjber <cjberragan@gmail.com>
2024-12-02 21:49:06 +0000

    add pii removal

--- planning_ai/nodes/map_node.py
+++ planning_ai/nodes/map_node.py
@@ -1,10 +1,26 @@
 from langgraph.constants import Send
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
 
 from planning_ai.chains.map_chain import map_chain
 from planning_ai.states import DocumentState, OverallState
 
+anonymizer = AnonymizerEngine()
+analyzer = AnalyzerEngine()
+
+
+def remove_pii(document: str):
+    results = analyzer.analyze(
+        text=document,
+        entities=["PERSON", "PHONE_NUMBER", "EMAIL_ADDRESS"],
+        language="en",
+    )
+    document = anonymizer.anonymize(text=document, analyzer_results=results)
+    return document
+
 
 def generate_summary(state: DocumentState):
+    state["document"] = remove_pii(state["document"])
     response = map_chain.invoke({"context": state["document"]})
     return {
         "summaries": [
